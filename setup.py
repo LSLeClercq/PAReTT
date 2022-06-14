2e067a0a8d855011a15ec23671141ea40140b7a5
Louis-Stéphane Le Clercq <85708751+LSLeClercq@users.noreply.github.com>
2022-06-15 00:28:41 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,5 +23,8 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
+    entry_points={ 
+        "console_scripts": [
+            "parett=parett:main"
     install_requires=["pandas", "numpy", "Bio", "splinter"],
 )
